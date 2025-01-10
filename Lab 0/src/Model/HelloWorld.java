71d3fe6346adbbbb9ce665cc91d447d52df11936
Sravan kumar Kurapati <sravankumarkurapati@Sravans-MacBook-Air.local>
2025-01-10 16:30:25 -0500

    Initial upload of Lab 0

---
+++ Lab 0/src/Model/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Sravan kumar Kurapati
+ * The purpose of this class is to print Hello, World
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello, World");
+    }
+}
