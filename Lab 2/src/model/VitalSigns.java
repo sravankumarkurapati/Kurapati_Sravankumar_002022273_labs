bbcd4b72db2ac30605c9c9aee5e307a087d59aba
Sravan kumar Kurapati <sravankumarkurapati@Sravans-MacBook-Air.local>
2025-01-24 23:25:36 -0500

    Initial upload of Lab 2 with VitalSigns class contains attributes to store vitals information. Project setup files are also uploaded

---
+++ Lab 2/src/model/VitalSigns.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ * The purpose of this class is to store details of Vitals. Setter and Getter methods have been provided
+ * @author sravankumarkurapati
+ * @version 1.0
+ * @since 2025-01-24
+ */
+public class VitalSigns {
+
+    String date;
+    double bloodPressure; // We can store numbers with decimals in double datatype variable
+    double temperature;
+    int pulse; // We can store whole numbers in integer datatype variable
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+}
