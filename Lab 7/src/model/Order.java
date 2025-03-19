40322f92b184cb277c951059bc0d2e677bda47b8
Sravan kumar Kurapati <sravankumarkurapati@Sravans-MacBook-Air.local>
2025-03-19 18:48:38 -0400

    Code changes made for adding model class of MasterOrderList, Order, OrderItem and high level Business class

---
+++ Lab 7/src/model/Order.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sravankumarkurapati
+ */
+public class Order {
+    
+    ArrayList<OrderItem> orderItemList;
+
+    public Order() {
+        this.orderItemList = new ArrayList<OrderItem>();
+    }
+
+    public ArrayList<OrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(ArrayList<OrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+    
+    public OrderItem addNewOrderItem(Product product, double price, int quantity){
+        OrderItem orderItem = new OrderItem(product,price,quantity);
+        orderItemList.add(orderItem);
+        return orderItem;
+    }
+}
